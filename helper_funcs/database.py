15cd52cfc5f4bc3405eb81e0de10e635d0126f81
Predator HackerzZ <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-06-20 22:02:03 +0530

    database.py

--- helper_funcs/database.py
+++ helper_funcs/database.py
@@ -3,4 +3,4 @@
 from sample_config import Config
 from helper_funcs.databasecq import Database
 
-db = Database(Config.MONGODB_URI, Config.SESSION_NAME)
+db = Database(sample_config.config.MONGODB_URI, sample_config.config.SESSION_NAME)
